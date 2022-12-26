3b5d99e9c31419420656174f568bb698c37a9242
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-26 16:58:50 +1100

    comparison instrucitons

--- src/lib/opcodes/instructions.rs
+++ src/lib/opcodes/instructions.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 /// Functions for each instruction
 ///
 /// Documentation from:
@@ -251,7 +253,113 @@ fn bvs_fn(cpu: &mut Cpu) -> u8 {
     }
 }
 
-// fn bit_fn() {
-//
-// }
+/// # Bit Test
+/// Perform a bitwise And between the accumulator and the value in memory.
+/// Tests if any of the bits are set (the result of the BitAnd is non-zero).
+/// The result is not stored back in memory or, the accumulator.
+/// Instead, is used to set processor status flags.
+/// ## Processor Status after use:
+/// - Z - Zero Flag         - Set if result is zero
+/// - N - Negative Flag     - Set if bit 7 of result is set
+/// - V - Overflow Flag     - Set if bit 6 of result is set
+fn bit_fn(cpu: &mut Cpu) -> u8 {
+    let fetched = cpu.fetch();
+    let result = cpu.a_register & fetched;
+
+    cpu.set_or_clear_flag(&CpuFlag::Zero, result == 0);
+    cpu.set_or_clear_flag(&CpuFlag::Negative, (fetched & (1 << 7)) != 0);
+    cpu.set_or_clear_flag(&CpuFlag::Overflow, (fetched & (1 << 6)) != 0);
+
+    0
+}
+
+/// # Clear Carry Flag
+/// Set the carry flag to 0
+/// ## Processor Status after use:
+/// - C - Carry Flag        - Set to 0
+fn clc_fn(cpu: &mut Cpu) -> u8 {
+    cpu.clear_flag(&CpuFlag::Carry);
+    0
+}
+
+/// # Clear Decimal Flag
+/// Set the decimal flag to 0
+/// ## Processor Status after use:
+/// - D - Decimal Flag      - Set to 0
+fn cld_fn(cpu: &mut Cpu) -> u8 {
+    cpu.clear_flag(&CpuFlag::Decimal);
+    0
+}
+
+/// # Clear Interrupt Disable
+/// Set the interrupt disable flag to 0
+/// ## Processor Status after use:
+/// - I - Interrupt Disable - Set to 0
+fn cli_fn(cpu: &mut Cpu) -> u8 {
+    cpu.clear_flag(&CpuFlag::Interrupt);
+    0
+}
+
+/// # Clear Overflow Flag
+/// Set the overflow flag to 0
+/// ## Processor Status after use:
+/// - V - Overflow Flag     - Set to 0
+fn clv_fn(cpu: &mut Cpu) -> u8 {
+    cpu.clear_flag(&CpuFlag::Overflow);
+    0
+}
+
+
+/// # Compare Accumultor
+/// Compares the contents of the accumulator with another value in memory (M).
+/// Result is returned by setting zero and carry flags.
+/// ## Processor Status after use:
+/// - C - Carry Flag        - Set if A >= M
+/// - Z - Zero Flag         - Set if A == M
+/// - N - Negative Flag     - Set if bit 7 of A - M is set
+/// ## Cycles:
+/// +1 if page crosses in certain addressing modes
+fn cmp_fn(cpu: &mut Cpu) -> u8 {
+    compare_values(cpu, cpu.a_register);
+    1
+}
+
+/// # Compare X Register
+/// Compares the contents of the X register with another value in memory (M).
+/// Result is returned by setting zero and carry flags.
+/// ## Processor Status after use:
+/// - C - Carry Flag        - Set if X >= M
+/// - Z - Zero Flag         - Set if X == M
+/// - N - Negative Flag     - Set if bit 7 of X - M is set
+fn cpx_fn(cpu: &mut Cpu) -> u8 {
+    compare_values(cpu, cpu.x_register);
+    0
+}
+
+
+/// # Compare Y Register
+/// Compares the contents of the Y register with another value in memory (M).
+/// Result is returned by setting zero and carry flags.
+/// ## Processor Status after use:
+/// - C - Carry Flag        - Set if Y >= M
+/// - Z - Zero Flag         - Set if Y == M
+/// - N - Negative Flag     - Set if bit 7 of Y - M is set
+fn cpy_fn(cpu: &mut Cpu) -> u8 {
+    compare_values(cpu, cpu.y_register);
+    0
+}
+
+/// Helper for compare instructions
+/// ## Processor Status after use:
+/// - C - Carry Flag        - Set if Register >= M
+/// - Z - Zero Flag         - Set if Register == M
+/// - N - Negative Flag     - Set if bit 7 of Register - M is set
+fn compare_values(cpu: &mut Cpu, register_val: u8) {
+    let rhs_val = cpu.fetch();
+    let result = ((register_val as u16 - rhs_val as u16) & 0x00FF) as u8;
+    cpu.set_or_clear_flag(&CpuFlag::Carry, register_val >= rhs_val);
+    cpu.set_or_clear_flag(&CpuFlag::Carry, result == 0);
+    cpu.set_or_clear_flag(&CpuFlag::Negative, result & 0x80 != 0);
+}
+
 
