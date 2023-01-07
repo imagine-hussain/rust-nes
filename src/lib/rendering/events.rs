b5cfa382aaae2aaa930f502334fdffa66d8acc07
imagine-hussain <hussain.nwz000@gmail.com>
2023-01-08 03:16:58 +1100

    add import events

--- src/lib/rendering/events.rs
+++ src/lib/rendering/events.rs
@@ -2,7 +2,6 @@ use sdl2::event::Event as SdlEvent;
 use sdl2::event::WindowEvent as SdlWindowEvent;
 
 type TimeStamp = u32;
-
 pub enum Event {
     Quit(TimeStamp),
     AppTerminating(TimeStamp),
@@ -14,6 +13,14 @@ pub enum Event {
     Display(DisplayEvent),
     Window(WindowEvent),
     Key(KeyEvent),
+    Text(TextEvent),
+    MouseMotion(MouseMotionEvent),
+    MouseClick(MouseClickEvent),
+    MouseWheel(MouseWheelEvent),
+    ClipboardUpdate(TimeStamp),
+    DropFile(DropFileEvent),
+    DropText(DropFileEvent),
+    Drop(DropEvent),
     Unknown(Option<TimeStamp>),
 }
 
@@ -31,12 +38,12 @@ impl From<SdlEvent> for Event {
             SdlEvent::Window { timestamp, window_id, win_event } => Self::Window(WindowEvent { timestamp, window_id, win_event}),
             SdlEvent::KeyDown { timestamp, window_id, keycode, scancode, keymod, repeat } => Self::Key(KeyEvent { timestamp, window_id, keycode, scancode, keymod, repeat, direction: PressDirection::Down}),
             SdlEvent::KeyUp { timestamp, window_id, keycode, scancode, keymod, repeat } => Self::Key(KeyEvent { timestamp, window_id, keycode, scancode, keymod, repeat, direction: PressDirection::Up}),
-            // SdlEvent::TextEditing { timestamp, window_id, text, start, length } => todo!(),
-            // SdlEvent::TextInput { timestamp, window_id, text } => todo!(),
-            // SdlEvent::MouseMotion { timestamp, window_id, which, mousestate, x, y, xrel, yrel } => todo!(),
-            // SdlEvent::MouseButtonDown { timestamp, window_id, which, mouse_btn, clicks, x, y } => todo!(),
-            // SdlEvent::MouseButtonUp { timestamp, window_id, which, mouse_btn, clicks, x, y } => todo!(),
-            // SdlEvent::MouseWheel { timestamp, window_id, which, x, y, direction } => todo!(),
+            SdlEvent::TextEditing { timestamp, window_id, text, start, length } => Self::Text(TextEvent { timestamp, window_id, text, offset: Some(SizedOffset {start, length}) }),
+            SdlEvent::TextInput { timestamp, window_id, text } => Self::Text(TextEvent { timestamp, window_id, text, offset: None }),
+            SdlEvent::MouseMotion { timestamp, window_id, which, mousestate, x, y, xrel, yrel } => Self::MouseMotion(MouseMotionEvent { timestamp, window_id, which, mousestate, x, y, xrel, yrel }),
+            SdlEvent::MouseButtonDown { timestamp, window_id, which, mouse_btn, clicks, x, y } => Self::MouseClick(MouseClickEvent { timestamp, window_id, which, mouse_btn, clicks, x, y, direction: PressDirection::Down }),
+            SdlEvent::MouseButtonUp { timestamp, window_id, which, mouse_btn, clicks, x, y } => Self::MouseClick(MouseClickEvent { timestamp, window_id, which, mouse_btn, clicks, x, y, direction: PressDirection::Up }),
+            SdlEvent::MouseWheel { timestamp, window_id, which, x, y, direction } => Self::MouseWheel(MouseWheelEvent { timestamp, window_id, which, x, y, direction }),
             // SdlEvent::JoyAxisMotion { timestamp, which, axis_idx, value } => todo!(),
             // SdlEvent::JoyBallMotion { timestamp, which, ball_idx, xrel, yrel } => todo!(),
             // SdlEvent::JoyHatMotion { timestamp, which, hat_idx, state } => todo!(),
@@ -56,11 +63,11 @@ impl From<SdlEvent> for Event {
             // SdlEvent::DollarGesture { timestamp, touch_id, gesture_id, num_fingers, error, x, y } => todo!(),
             // SdlEvent::DollarRecord { timestamp, touch_id, gesture_id, num_fingers, error, x, y } => todo!(),
             // SdlEvent::MultiGesture { timestamp, touch_id, d_theta, d_dist, x, y, num_fingers } => todo!(),
-            // SdlEvent::ClipboardUpdate { timestamp } => todo!(),
-            // SdlEvent::DropFile { timestamp, window_id, filename } => todo!(),
-            // SdlEvent::DropText { timestamp, window_id, filename } => todo!(),
-            // SdlEvent::DropBegin { timestamp, window_id } => todo!(),
-            // SdlEvent::DropComplete { timestamp, window_id } => todo!(),
+            SdlEvent::ClipboardUpdate { timestamp } => Self::ClipboardUpdate(timestamp),
+            SdlEvent::DropFile { timestamp, window_id, filename } => Self::DropFile(DropFileEvent { timestamp, window_id, filename }),
+            SdlEvent::DropText { timestamp, window_id, filename } => Self::DropText(DropFileEvent { timestamp, window_id, filename }),
+            SdlEvent::DropBegin { timestamp, window_id } => Self::Drop(DropEvent { timestamp, window_id, stage: DropStage::Begin }),
+            SdlEvent::DropComplete { timestamp, window_id } => Self::Drop(DropEvent { timestamp, window_id, stage: DropStage::Complete }),
             // SdlEvent::AudioDeviceAdded { timestamp, which, iscapture } => todo!(),
             // SdlEvent::AudioDeviceRemoved { timestamp, which, iscapture } => todo!(),
             // SdlEvent::RenderTargetsReset { timestamp } => todo!(),
@@ -98,4 +105,65 @@ pub struct KeyEvent {
     pub repeat: bool,
     pub direction: PressDirection,
 }
+
+pub struct TextEvent {
+    pub timestamp: TimeStamp,
+    pub window_id: u32,
+    pub text: String,
+    pub offset: Option<SizedOffset>,
+}
+
+pub struct SizedOffset  {
+    pub start: i32,
+    pub length: i32,
+}
+
+pub struct MouseMotionEvent {
+    pub timestamp: TimeStamp,
+    pub window_id: u32,
+    pub which: u32,
+    pub mousestate: sdl2::mouse::MouseState,
+    pub x: i32,
+    pub y: i32,
+    pub xrel: i32,
+    pub yrel: i32,
+}
+
+pub struct MouseClickEvent {
+    pub timestamp: TimeStamp,
+    pub window_id: u32,
+    pub which: u32,
+    pub mouse_btn: sdl2::mouse::MouseButton,
+    pub clicks: u8,
+    pub x: i32,
+    pub y: i32,
+    pub direction: PressDirection,
+}
+
+pub struct MouseWheelEvent {
+    pub timestamp: TimeStamp,
+    pub window_id: u32,
+    pub which: u32,
+    pub x: i32,
+    pub y: i32,
+    pub direction: sdl2::mouse::MouseWheelDirection,
+}
+
+pub struct DropFileEvent {
+    pub timestamp: TimeStamp,
+    pub window_id: u32,
+    pub filename: String,
+}
+
+pub enum DropStage {
+    Begin,
+    Complete
+}
+
+pub struct DropEvent {
+    pub timestamp: TimeStamp,
+    pub window_id: u32,
+    pub stage: DropStage,
+}
+
 
