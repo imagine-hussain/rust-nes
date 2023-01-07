d37f2810afd2487ba1ce91b5dba55e4290de29e4
imagine-hussain <hussain.nwz000@gmail.com>
2023-01-08 03:29:53 +1100

    more events impl

--- src/lib/rendering/events.rs
+++ src/lib/rendering/events.rs
@@ -1,3 +1,5 @@
+use std::ffi::c_void;
+
 use sdl2::event::Event as SdlEvent;
 use sdl2::event::WindowEvent as SdlWindowEvent;
 
@@ -21,34 +23,231 @@ pub enum Event {
     DropFile(DropFileEvent),
     DropText(DropFileEvent),
     Drop(DropEvent),
-    Unknown(Option<TimeStamp>),
+    JoyButton(JoyButtonEvent),
+    JoyAxisMotion(JoyAxisMotionEvent),
+    JoyBallMotion(JoyBallMotionEvent),
+    JoyHatMotion(JoyHatMotionEvent),
+    AudioDeviceAdded(AudioDeviceEvent),
+    AudioDeviceRemoved(AudioDeviceEvent),
+    RenderDeviceReset(TimeStamp),
+    RenderTargetsReset(TimeStamp),
+    User(UserEvent),
+    Unknown(UnknownEvent),
 }
 
 impl From<SdlEvent> for Event {
+    // Partial implementation for the important events
+    // TODO: Implement the rest aswell
     fn from(event: SdlEvent) -> Self {
         match event {
             SdlEvent::Quit { timestamp } => Self::Quit(timestamp),
             SdlEvent::AppTerminating { timestamp } => Self::AppTerminating(timestamp),
             SdlEvent::AppLowMemory { timestamp } => Self::AppLowMemory(timestamp),
-            SdlEvent::AppWillEnterBackground { timestamp } => Self::AppWillEnterBackground(timestamp),
+            SdlEvent::AppWillEnterBackground { timestamp } => {
+                Self::AppWillEnterBackground(timestamp)
+            }
             SdlEvent::AppDidEnterBackground { timestamp } => Self::AppDidEnterBackground(timestamp),
-            SdlEvent::AppWillEnterForeground { timestamp } => Self::AppWillEnterForeground(timestamp),
+            SdlEvent::AppWillEnterForeground { timestamp } => {
+                Self::AppWillEnterForeground(timestamp)
+            }
             SdlEvent::AppDidEnterForeground { timestamp } => Self::AppDidEnterBackground(timestamp),
-            SdlEvent::Display { timestamp, display_index, display_event } => Self::Display(DisplayEvent { timestamp, display_index, display_event }),
-            SdlEvent::Window { timestamp, window_id, win_event } => Self::Window(WindowEvent { timestamp, window_id, win_event}),
-            SdlEvent::KeyDown { timestamp, window_id, keycode, scancode, keymod, repeat } => Self::Key(KeyEvent { timestamp, window_id, keycode, scancode, keymod, repeat, direction: PressDirection::Down}),
-            SdlEvent::KeyUp { timestamp, window_id, keycode, scancode, keymod, repeat } => Self::Key(KeyEvent { timestamp, window_id, keycode, scancode, keymod, repeat, direction: PressDirection::Up}),
-            SdlEvent::TextEditing { timestamp, window_id, text, start, length } => Self::Text(TextEvent { timestamp, window_id, text, offset: Some(SizedOffset {start, length}) }),
-            SdlEvent::TextInput { timestamp, window_id, text } => Self::Text(TextEvent { timestamp, window_id, text, offset: None }),
-            SdlEvent::MouseMotion { timestamp, window_id, which, mousestate, x, y, xrel, yrel } => Self::MouseMotion(MouseMotionEvent { timestamp, window_id, which, mousestate, x, y, xrel, yrel }),
-            SdlEvent::MouseButtonDown { timestamp, window_id, which, mouse_btn, clicks, x, y } => Self::MouseClick(MouseClickEvent { timestamp, window_id, which, mouse_btn, clicks, x, y, direction: PressDirection::Down }),
-            SdlEvent::MouseButtonUp { timestamp, window_id, which, mouse_btn, clicks, x, y } => Self::MouseClick(MouseClickEvent { timestamp, window_id, which, mouse_btn, clicks, x, y, direction: PressDirection::Up }),
-            SdlEvent::MouseWheel { timestamp, window_id, which, x, y, direction } => Self::MouseWheel(MouseWheelEvent { timestamp, window_id, which, x, y, direction }),
-            // SdlEvent::JoyAxisMotion { timestamp, which, axis_idx, value } => todo!(),
-            // SdlEvent::JoyBallMotion { timestamp, which, ball_idx, xrel, yrel } => todo!(),
-            // SdlEvent::JoyHatMotion { timestamp, which, hat_idx, state } => todo!(),
-            // SdlEvent::JoyButtonDown { timestamp, which, button_idx } => todo!(),
-            // SdlEvent::JoyButtonUp { timestamp, which, button_idx } => todo!(),
+            SdlEvent::Display {
+                timestamp,
+                display_index,
+                display_event,
+            } => Self::Display(DisplayEvent {
+                timestamp,
+                display_index,
+                display_event,
+            }),
+            SdlEvent::Window {
+                timestamp,
+                window_id,
+                win_event,
+            } => Self::Window(WindowEvent {
+                timestamp,
+                window_id,
+                win_event,
+            }),
+            SdlEvent::KeyDown {
+                timestamp,
+                window_id,
+                keycode,
+                scancode,
+                keymod,
+                repeat,
+            } => Self::Key(KeyEvent {
+                timestamp,
+                window_id,
+                keycode,
+                scancode,
+                keymod,
+                repeat,
+                direction: PressDirection::Down,
+            }),
+            SdlEvent::KeyUp {
+                timestamp,
+                window_id,
+                keycode,
+                scancode,
+                keymod,
+                repeat,
+            } => Self::Key(KeyEvent {
+                timestamp,
+                window_id,
+                keycode,
+                scancode,
+                keymod,
+                repeat,
+                direction: PressDirection::Up,
+            }),
+            SdlEvent::TextEditing {
+                timestamp,
+                window_id,
+                text,
+                start,
+                length,
+            } => Self::Text(TextEvent {
+                timestamp,
+                window_id,
+                text,
+                offset: Some(SizedOffset { start, length }),
+            }),
+            SdlEvent::TextInput {
+                timestamp,
+                window_id,
+                text,
+            } => Self::Text(TextEvent {
+                timestamp,
+                window_id,
+                text,
+                offset: None,
+            }),
+            SdlEvent::MouseMotion {
+                timestamp,
+                window_id,
+                which,
+                mousestate,
+                x,
+                y,
+                xrel,
+                yrel,
+            } => Self::MouseMotion(MouseMotionEvent {
+                timestamp,
+                window_id,
+                which,
+                mousestate,
+                x,
+                y,
+                xrel,
+                yrel,
+            }),
+            SdlEvent::MouseButtonDown {
+                timestamp,
+                window_id,
+                which,
+                mouse_btn,
+                clicks,
+                x,
+                y,
+            } => Self::MouseClick(MouseClickEvent {
+                timestamp,
+                window_id,
+                which,
+                mouse_btn,
+                clicks,
+                x,
+                y,
+                direction: PressDirection::Down,
+            }),
+            SdlEvent::MouseButtonUp {
+                timestamp,
+                window_id,
+                which,
+                mouse_btn,
+                clicks,
+                x,
+                y,
+            } => Self::MouseClick(MouseClickEvent {
+                timestamp,
+                window_id,
+                which,
+                mouse_btn,
+                clicks,
+                x,
+                y,
+                direction: PressDirection::Up,
+            }),
+            SdlEvent::MouseWheel {
+                timestamp,
+                window_id,
+                which,
+                x,
+                y,
+                direction,
+            } => Self::MouseWheel(MouseWheelEvent {
+                timestamp,
+                window_id,
+                which,
+                x,
+                y,
+                direction,
+            }),
+            SdlEvent::JoyAxisMotion {
+                timestamp,
+                which,
+                axis_idx,
+                value,
+            } => Self::JoyAxisMotion(JoyAxisMotionEvent {
+                timestamp,
+                which,
+                axis_idx,
+                value,
+            }),
+            SdlEvent::JoyBallMotion {
+                timestamp,
+                which,
+                ball_idx,
+                xrel,
+                yrel,
+            } => Self::JoyBallMotion(JoyBallMotionEvent {
+                timestamp,
+                which,
+                ball_idx,
+                xrel,
+                yrel,
+            }),
+            SdlEvent::JoyHatMotion {
+                timestamp,
+                which,
+                hat_idx,
+                state,
+            } => Self::JoyHatMotion(JoyHatMotionEvent {
+                timestamp,
+                which,
+                hat_idx,
+                state,
+            }),
+            SdlEvent::JoyButtonDown {
+                timestamp,
+                which,
+                button_idx,
+            } => Self::JoyButton(JoyButtonEvent {
+                timestamp,
+                which,
+                button_idx,
+                direction: PressDirection::Down,
+            }),
+            SdlEvent::JoyButtonUp {
+                timestamp,
+                which,
+                button_idx,
+            } => Self::JoyButton(JoyButtonEvent {
+                timestamp,
+                which,
+                button_idx,
+                direction: PressDirection::Up,
+            }),
             // SdlEvent::JoyDeviceAdded { timestamp, which } => todo!(),
             // SdlEvent::JoyDeviceRemoved { timestamp, which } => todo!(),
             // SdlEvent::ControllerAxisMotion { timestamp, which, axis, value } => todo!(),
@@ -64,17 +263,83 @@ impl From<SdlEvent> for Event {
             // SdlEvent::DollarRecord { timestamp, touch_id, gesture_id, num_fingers, error, x, y } => todo!(),
             // SdlEvent::MultiGesture { timestamp, touch_id, d_theta, d_dist, x, y, num_fingers } => todo!(),
             SdlEvent::ClipboardUpdate { timestamp } => Self::ClipboardUpdate(timestamp),
-            SdlEvent::DropFile { timestamp, window_id, filename } => Self::DropFile(DropFileEvent { timestamp, window_id, filename }),
-            SdlEvent::DropText { timestamp, window_id, filename } => Self::DropText(DropFileEvent { timestamp, window_id, filename }),
-            SdlEvent::DropBegin { timestamp, window_id } => Self::Drop(DropEvent { timestamp, window_id, stage: DropStage::Begin }),
-            SdlEvent::DropComplete { timestamp, window_id } => Self::Drop(DropEvent { timestamp, window_id, stage: DropStage::Complete }),
-            // SdlEvent::AudioDeviceAdded { timestamp, which, iscapture } => todo!(),
-            // SdlEvent::AudioDeviceRemoved { timestamp, which, iscapture } => todo!(),
-            // SdlEvent::RenderTargetsReset { timestamp } => todo!(),
-            // SdlEvent::RenderDeviceReset { timestamp } => todo!(),
-            // SdlEvent::User { timestamp, window_id, type_, code, data1, data2 } => todo!(),
-            // SdlEvent::Unknown { timestamp, type_ } => todo!(),
-            _ => Self::Unknown(None),
+            SdlEvent::DropFile {
+                timestamp,
+                window_id,
+                filename,
+            } => Self::DropFile(DropFileEvent {
+                timestamp,
+                window_id,
+                filename,
+            }),
+            SdlEvent::DropText {
+                timestamp,
+                window_id,
+                filename,
+            } => Self::DropText(DropFileEvent {
+                timestamp,
+                window_id,
+                filename,
+            }),
+            SdlEvent::DropBegin {
+                timestamp,
+                window_id,
+            } => Self::Drop(DropEvent {
+                timestamp,
+                window_id,
+                stage: DropStage::Begin,
+            }),
+            SdlEvent::DropComplete {
+                timestamp,
+                window_id,
+            } => Self::Drop(DropEvent {
+                timestamp,
+                window_id,
+                stage: DropStage::Complete,
+            }),
+            SdlEvent::AudioDeviceAdded {
+                timestamp,
+                which,
+                iscapture,
+            } => Self::AudioDeviceAdded(AudioDeviceEvent {
+                timestamp,
+                which,
+                iscapture,
+            }),
+            SdlEvent::AudioDeviceRemoved {
+                timestamp,
+                which,
+                iscapture,
+            } => Self::AudioDeviceRemoved(AudioDeviceEvent {
+                timestamp,
+                which,
+                iscapture,
+            }),
+            SdlEvent::RenderTargetsReset { timestamp } => Self::RenderTargetsReset(timestamp),
+            SdlEvent::RenderDeviceReset { timestamp } => Self::RenderDeviceReset(timestamp),
+            SdlEvent::User {
+                timestamp,
+                window_id,
+                type_,
+                code,
+                data1,
+                data2,
+            } => Self::User(UserEvent {
+                timestamp,
+                window_id,
+                type_,
+                code,
+                data1,
+                data2,
+            }),
+            SdlEvent::Unknown { timestamp, type_ } => Self::Unknown(UnknownEvent {
+                timestamp: Some(timestamp),
+                type_: Some(type_),
+            }),
+            _ => Self::Unknown(UnknownEvent {
+                timestamp: None,
+                type_: None,
+            }),
         }
     }
 }
@@ -82,7 +347,7 @@ impl From<SdlEvent> for Event {
 pub struct DisplayEvent {
     pub timestamp: TimeStamp,
     pub display_index: i32,
-    pub display_event: sdl2::event::DisplayEvent
+    pub display_event: sdl2::event::DisplayEvent,
 }
 
 pub struct WindowEvent {
@@ -113,7 +378,7 @@ pub struct TextEvent {
     pub offset: Option<SizedOffset>,
 }
 
-pub struct SizedOffset  {
+pub struct SizedOffset {
     pub start: i32,
     pub length: i32,
 }
@@ -157,7 +422,7 @@ pub struct DropFileEvent {
 
 pub enum DropStage {
     Begin,
-    Complete
+    Complete,
 }
 
 pub struct DropEvent {
@@ -166,4 +431,51 @@ pub struct DropEvent {
     pub stage: DropStage,
 }
 
+pub struct JoyButtonEvent {
+    pub timestamp: TimeStamp,
+    pub which: u32,
+    pub button_idx: u8,
+    pub direction: PressDirection,
+}
 
+pub struct JoyAxisMotionEvent {
+    pub timestamp: TimeStamp,
+    pub which: u32,
+    pub axis_idx: u8,
+    pub value: i16,
+}
+
+pub struct JoyBallMotionEvent {
+    pub timestamp: TimeStamp,
+    pub which: u32,
+    pub ball_idx: u8,
+    pub xrel: i16,
+    pub yrel: i16,
+}
+
+pub struct JoyHatMotionEvent {
+    pub timestamp: TimeStamp,
+    pub which: u32,
+    pub hat_idx: u8,
+    pub state: sdl2::joystick::HatState,
+}
+
+pub struct AudioDeviceEvent {
+    pub timestamp: TimeStamp,
+    pub which: u32,
+    pub iscapture: bool,
+}
+
+pub struct UserEvent {
+    pub timestamp: TimeStamp,
+    pub window_id: u32,
+    pub type_: u32,
+    pub code: i32,
+    pub data1: *mut c_void,
+    pub data2: *mut c_void,
+}
+
+pub struct UnknownEvent {
+    pub timestamp: Option<TimeStamp>,
+    pub type_: Option<u32>,
+}
